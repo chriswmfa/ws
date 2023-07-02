19a31d116549e5e7b7be30ba1b0eaf83152e6fcb
chriswmfa <bapsteks@gmail.com>
2023-07-03 00:16:17 +0100

    Cleaner version - added type signature compatibility

--- memoizer.ts
+++ memoizer.ts
@@ -1,26 +1,17 @@
-function memoize<T extends (...args: any[]) => any>(func: T): T {
-    // Create a cache object to store the computed results
-    const cache: Map<string, any> = new Map();
-  
-    // Return a new function that acts as the memoized version of the input function
-    return function (...args: any[]): any {
-      // Generate a unique key based on the function arguments
-      const key: string = JSON.stringify(args);
-  
-      // Check if the result is already cached
-      if (cache.has(key)) {
-        // Return the cached result
-        return cache.get(key);
-      }
-  
-      // Call the original function with the provided arguments
-      const result: any = func.apply(this, args);
-  
-      // Cache the result for future use
-      cache.set(key, result);
-  
-      // Return the computed result
-      return result;
-    } as T;
-  }
-  
+type Func<T extends any[], R> = (...args: T) => R;
+
+function memoize<T extends any[], R>(func: Func<T, R>): Func<T, R> {
+  const cache = new Map<string, R>();
+
+  return (...args: T): R => {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key)!;
+    }
+
+    const result = func(...args);
+    cache.set(key, result);
+    return result;
+  };
+}
